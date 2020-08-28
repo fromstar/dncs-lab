3c70dee1910ad5daf4984520f09213d92173da32
fromstar <dallastella.davide@gmail.com>
2020-08-28 17:12:46 +0200

    Delete host-c1.sh

--- host-c1.sh
+++
@@ -1,9 +0,0 @@
-export DEBIAN_FRONTEND=noninteractive
-
-sudo apt-get update
-
-sudo snap install docker
-sudo apt-get upgrade
-
-sudo docker pull nginx
-sudo docker run --name webserver -p 80:80 -d nginx
